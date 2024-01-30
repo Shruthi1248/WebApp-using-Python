e3d3ca4827413940ddc3a2bb0a57902e36054935
Shruthi <shruthii1248@gmail.com>
2024-01-30 14:20:16 +0530

    Second Commit

--- WebProject/settings.py
+++ WebProject/settings.py
@@ -118,9 +118,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR,'static')
+    os.path.join(BASE_DIR,'')
 ]
-STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+#STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
