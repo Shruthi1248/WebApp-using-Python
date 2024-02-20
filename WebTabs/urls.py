2a490c626716c4ea42d068c3e9da1e1d633d81b6
Shruthi S <149942843+Shruthi1248@users.noreply.github.com>
2024-02-20 15:23:12 +0530

    Fourth Commit

--- WebTabs/urls.py
+++ WebTabs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns =[
-    path('', views.home, name="home")
+urlpatterns = [
+    path('', views.home, name="home"),
+    path('submit_form_data/', views.submit_form_data, name='submit_form_data'),
+    path('submit_field_data/', views.submit_field_data, name='submit_field_data'),
 ]
